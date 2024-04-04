8aee23a9596a1a231cda9fb821e354ad3bc09022
mykomiparanum <m.miparanum@gmail.com>
2024-04-04 10:21:37 +0200

    added utility method & error handling

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,24 +1,52 @@
+import { Logger } from '@nestjs/common';
 import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+interface ChatMessage {
+  name: string;
+  message: string;
+}
 
 @WebSocketGateway({ cors: '*' })
-
-// @WebSocketGateway(8001, {
-//   cors: { origin: 'https://guessing-game-frontend.onrender.com' },
-// })
-export class ChatGateway {
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   @WebSocketServer()
-  server;
+  server: Server;
+
+  private logger: Logger = new Logger('ChatGateway');
+
+  afterInit(server: Server) {
+    this.logger.log('WebSocket initialized');
+  }
+
+  handleConnection(client: Socket) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
 
   @SubscribeMessage('message')
-  handleMessage(
-    @MessageBody() message: { name: string; message: string },
-  ): void {
-    // console.log(message);
-    this.server.emit('message', message);
+  handleMessage(@MessageBody() message: ChatMessage): void {
+    try {
+      this.broadcast('message', message);
+    } catch (error) {
+      this.logger.error(`Error broadcasting message: ${error.message}`);
+    }
+  }
+
+  private broadcast(event: string, message: any): void {
+    this.server.emit(event, message); // Emitting message to all connected clients
+    this.logger.log(`Broadcasting message: ${JSON.stringify(message)}`);
   }
 }
